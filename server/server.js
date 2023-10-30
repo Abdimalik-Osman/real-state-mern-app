ec80da9cf91b6eb5c58e26a22a2075975fbf4ad6
Abdimalik-Osman <engabdimalik8818@gmail.com>
2023-10-30 20:56:55 +0300

    database connection established

--- server/server.js
+++ server/server.js
@@ -1,5 +1,12 @@
 const express = require("express");
-
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DB_URL)
+.then(()=>{
+    console.log("connection established successfully");
+})
+.catch(err => console(err.message));
 const app = express();
 
 app.listen(4000,()=>{
