faa7626044ee256b495f207ee89e281d3155773c
Abdimalik-Osman <engabdimalik8818@gmail.com>
2023-10-31 20:53:28 +0300

    login api created

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const bcryptjs = require('bcryptjs');
 const User = require("../models/userModel");
 const  errorHandler  = require('../utils/error');
-
+const jwt = require("jsonwebtoken")
 exports.SingUp = async(req,res,next)=>{
 try {
     console.log("here")
@@ -22,4 +22,23 @@ try {
     next(errorHandler(500,error.message));
     // return res.status(500).json({message: error.message,status:"failed"}); 
 }
+}
+
+// sign in 
+exports.LogIn = async(req, res, next) => {
+    try {
+        const {username, password} = req.body
+        const user = await User.findOne({ username: username})
+        if(!user) return next(errorHandler(404,'User not found'));
+        const validPassword = bcryptjs.compareSync(password, user.password);
+
+        if (!validPassword) return next(errorHandler(404,"Invalid credentials"));
+        // this line is used to leave the password field
+        const {password:pass, ...rest} = user._doc; 
+        const token = jwt.sign({id: user._id},process.env.JWT_SECRET);
+
+        res.cookie("access_token",token,{httpOnly: true }).json(rest);
+    } catch (error) {
+        next(errorHandler(500,error));
+    }
 }
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -5,4 +5,6 @@ const router = express.Router();
 
 //sign up
 router.post('/signup',AuthController.SingUp )
+// login
+router.post('/login',AuthController.LogIn )
 module.exports = router
