595f66e889cd940d5b4c56a72c4e0bf101b79517
Abdimalik-Osman <engabdimalik8818@gmail.com>
2023-11-04 21:36:10 +0300

    profile page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Header from "./Components/Header"
+import PrivateRoute from "./Components/PrivateRoute"
 import About from "./pages/About"
 import Profile from "./pages/Profile"
 import SignIn from "./pages/SignIn"
@@ -17,7 +18,9 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<Signup />} />
         <Route path="/sign-out" element={<SignOut />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
